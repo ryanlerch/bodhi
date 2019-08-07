bb5071f51b8ed11ebbdeeb8b211fd00507224703
Ryan Lerch <rlerch@redhat.com>
2019-08-07 16:10:13 +1000

    pull bugzilla into the bug chooser options from the builds list

--- bodhi/server/static/js/update_form.js
+++ bodhi/server/static/js/update_form.js
@@ -25,57 +25,28 @@ $(document).ready(function() {
     }
 
     var messenger = Messenger({theme: 'flat'});
-    var buildssearchterm = "";
-    var $builds_search_selectize = $('#builds-search').selectize({
-        valueField: 'nvr',
-        labelField: 'nvr',
-        searchField: ['nvr', 'tag_name', 'owner_name'],
-        preload: true,
-        plugins: ['remove_button','restore_on_backspace'],
-        render: {
-            option: function(item, escape) {
-                return '<div class="w-100 border-bottom px-1">' +
-                '   <h6 class="font-weight-bold mb-0">' + escape(item.nvr) + '</h6>' +
-                '   <span class="badge badge-light border"><i class="fa fa-tag"></i> '+escape(item.release_name)+'</span> '+
-                '   <span class="badge badge-light border"><i class="fa fa-user"></i> '+escape(item.owner_name)+'</span> '+
-                '</div>';
-            },
-            item: function(item, escape) {
-                return '<div class="w-100 border-bottom m-0 py-1 pl-3">' +
-                       '   <span class="name">' + escape(item.nvr) + '</span>' +
-                       '   <span class="badge badge-light border float-right">' + escape(item.release_name) + '</span>' +
-                       '</div>';
-            },
-        },
-        onItemAdd: function(value, item){
-            $builds_search_selectize.setTextboxValue(buildssearchterm)
-            $builds_search_selectize.refreshOptions(true)
-            $builds_search_selectize.updatePlaceholder()
-        },
-        onType: function(searchterm){
-            buildssearchterm = searchterm;
-        },
-        onBlur: function(){
-            $('#builds-search-selectized').attr("placeholder", $builds_search_selectize.settings.placeholder);
-        },
-        onFocus: function(){
-            $('#builds-search-selectized').attr("placeholder", "");
-        },
-        load: function(query, callback) {
+
+    // function to load the list of bug options for the bug chooser.
+    function loadBugs(packagename) {
+        // generate products list for the URL from settings.bz_products (defined globally in master.html)
+        var products_string = "";
+        for (var p in settings.bz_products){
+            products_string = products_string+'&product='+encodeURIComponent(settings.bz_products[p]);
+        }
+        return function(callback){
             $.ajax({
-                url: '/latest_candidates?prefix=' + encodeURIComponent(query),
+                url: 'https://bugzilla.redhat.com/rest/bug?bug_status=__open__&component='+packagename+products_string+'&include_fields=summary,status,id,product,component,version',
                 type: 'GET',
+                dataType: 'jsonp',
                 error: function() {
                     callback();
                 },
                 success: function(res) {
-                    callback(res);
+                    callback(res.bugs);
                 }
             });
         }
-    });
-
-    $builds_search_selectize = $builds_search_selectize[0].selectize;
+    }
 
     var $bugs_search_selectize = $('#bugs-search').selectize( {
         create: function(input, callback){
@@ -119,8 +90,13 @@ $(document).ready(function() {
         },
         valueField: 'id',
         labelField: 'id',
+        searchField: ['id', 'summary', 'component'],
         createFilter: "^[0-9]+$",
         plugins: ['remove_button','restore_on_backspace'],
+        onInitialize: function(){
+            // make sure the placeholder shows when items already exist when page loads
+            $('#bugs-search-selectized').attr("placeholder", "search and add bugs");
+        },
         onBlur: function(){
             $('#bugs-search-selectized').attr("placeholder", $bugs_search_selectize.settings.placeholder);
         },
@@ -134,10 +110,91 @@ $(document).ready(function() {
                 '   <span class="name" title="bug description">' + escape(item.summary) + '</span>' +
                 '</div>';
             },
+            option: function(item, escape) {
+                return '<div class="w-100 border-bottom m-0 py-1 pl-3">' +
+                '<div>' +
+                '   <span class="font-weight-bold" title="bug description">BZ#' + escape(item.id) + '</span>' +
+                '   <span class="name" title="bug description">' + escape(item.summary) + '</span>' +
+                '</div>'+
+                '<div>' +
+                '   <span class="badge badge-light border">' + escape(item.component[0]) + '</span>' +
+                '   <span class="badge badge-light border">' + escape(item.product) + ' '+escape(item.version[0])+'</span>' +
+                '</div>'+
+                '</div>';
+            },
         },
     });
-
     $bugs_search_selectize = $bugs_search_selectize[0].selectize;
+
+    var buildssearchterm = "";
+    var $builds_search_selectize = $('#builds-search').selectize({
+        valueField: 'nvr',
+        labelField: 'nvr',
+        searchField: ['nvr', 'tag_name', 'owner_name'],
+        preload: true,
+        plugins: ['remove_button','restore_on_backspace'],
+        render: {
+            option: function(item, escape) {
+                return '<div class="w-100 border-bottom px-1">' +
+                '   <h6 class="font-weight-bold mb-0">' + escape(item.nvr) + '</h6>' +
+                '   <span class="badge badge-light border"><i class="fa fa-tag"></i> '+escape(item.release_name)+'</span> '+
+                '   <span class="badge badge-light border"><i class="fa fa-user"></i> '+escape(item.owner_name)+'</span> '+
+                '</div>';
+            },
+            item: function(item, escape) {
+                return '<div class="w-100 border-bottom m-0 py-1 pl-3">' +
+                       '   <span class="name">' + escape(item.nvr) + '</span>' +
+                       '   <span class="badge badge-light border float-right">' + escape(item.release_name) + '</span>' +
+                       '</div>';
+            },
+        },
+        onItemAdd: function(value, item){
+            $builds_search_selectize.setTextboxValue(buildssearchterm)
+            $builds_search_selectize.refreshOptions(true)
+            $builds_search_selectize.updatePlaceholder()
+
+            // when adding a new build, pull the bugs into the bugs chooser options
+            $bugs_search_selectize.load(loadBugs(this.options[value].package_name))
+        },
+        onType: function(searchterm){
+            buildssearchterm = searchterm;
+        },
+        onBlur: function(){
+            // make sure the placeholder reappears when focus is lost
+            $('#builds-search-selectized').attr("placeholder", $builds_search_selectize.settings.placeholder);
+        },
+        onInitialize: function(){
+            // make sure the placeholder shows when items already exist when page loads
+            $('#builds-search-selectized').attr("placeholder", "search and add builds");
+
+            // preload bugs from builds that exist when the page loads (i.e. when editing an existing update)
+            for (var b in this.options) {
+                if (this.options.hasOwnProperty(b)) {
+                    $bugs_search_selectize.load(loadBugs(this.options[b].package_name))
+                }
+
+            }
+            
+        },
+        onFocus: function(){
+            // make sure the placeholder disappears when focused
+            $('#builds-search-selectized').attr("placeholder", "");
+        },
+        load: function(query, callback) {
+            $.ajax({
+                url: '/latest_candidates?prefix=' + encodeURIComponent(query),
+                type: 'GET',
+                error: function() {
+                    callback();
+                },
+                success: function(res) {
+                    callback(res);
+                }
+            });
+        }
+    });
+
+    $builds_search_selectize = $builds_search_selectize[0].selectize;
 
     $('#updatetypes').selectize();
     $('#severity').selectize();
